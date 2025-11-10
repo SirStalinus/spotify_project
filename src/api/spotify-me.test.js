01d5e479cdabdfed33a6db5d8d0028d507d247d3
MONICHON SIRONNEAU MAXIME <mmonicho@peda.pun.univ-poitiers.fr>
2025-11-10 18:15:23 +0100

    feat: ✨ add sonar analysis final

--- src/api/spotify-me.test.js
+++ src/api/spotify-me.test.js
@@ -16,7 +16,7 @@ describe("spotify-me API", () => {
     test("returns error if no token is provided", async () => {
       const result = await fetchAccountProfile("");
       expect(result).toEqual({
-        error: "No access token found.",
+        error: "No accss token found.",
         profile: null,
       });
     });
