b9791f9d7ab64c664acb86610cc4afdc1c9d017a
SirStalinus <github-action@actions-template-sync.noreply.github.com>
2025-12-01 01:14:21 +0000

    chore(template): merge template changes :up:
    Signed-off-by: SirStalinus <github-action@actions-template-sync.noreply.github.com>

--- src/components/TopArtistItem/TopArtistItem.test.jsx
+++ src/components/TopArtistItem/TopArtistItem.test.jsx
@@ -12,7 +12,7 @@ describe('TopArtistItem component', () => {
             name: 'Test Artist',
             images: [{ url: 'test.jpg' }, { url: 'test-medium.jpg' }, { url: 'test-small.jpg' }],
             genres: ['pop', 'rock'],
-            followers: { total: 100 },
+            followers: { total: 1000 },
             popularity: 85,
             external_urls: { spotify: 'https://open.spotify.com/artist/artist1' }
         };
@@ -70,24 +70,5 @@ describe('TopArtistItem component', () => {
 
         // uncomment to debug
         //screen.debug();
-    });
-
-    test('index starts at 1 when passed index 0', () => {
-        const artist = {
-            id: 'artist3',
-            name: 'Index Artist',
-            images: [{ url: 'idx.jpg' }],
-            genres: [],
-            followers: { total: 0 },
-            external_urls: { spotify: 'https://open.spotify.com/artist/artist3' }
-        };
-        render(<TopArtistItem artist={artist} index={0} />);
-
-        const listItem = screen.getByTestId(`top-artist-item-${artist.id}`);
-        expect(listItem).toBeInTheDocument();
-
-        // expect the displayed index to be "1" (index prop is 0 -> displayed as 1)
-        const indexNode = within(listItem).getByText(/\b1\b/);
-        expect(indexNode).toBeInTheDocument();
     });
 });
